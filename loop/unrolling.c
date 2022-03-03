01dc27a1a0e93a2308aa8871715281494a40ee25
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-03-03 23:13:05 +0800

    include O1 result
    add side effect to ensure O1 don't eliminate dead code, but this also
    changes O0's result

--- loop/unrolling.c
+++ loop/unrolling.c
@@ -8,32 +8,36 @@ int f(int i) { return i * i; }
 int main() {
   clock_t start, end;
   double cpu_time_used;
+  int sum;
 
   start = clock();
+  sum = 0;
   for (int i = 0; i < N; i++) {
-    f(i);
+    sum += f(i);
   }
   end = clock();
-  printf("Time measured: %.3f seconds.\n",
-         ((double)(end - start)) / CLOCKS_PER_SEC);
+  printf("Time measured: %.3f seconds. Sum: %d\n",
+         ((double)(end - start)) / CLOCKS_PER_SEC, sum);
 
   start = clock();
+  sum = 0;
   for (int i = 0; i < N; i += 2) {
-    f(i);
-    f(i + 1);
+    sum += f(i);
+    sum += f(i + 1);
   }
   end = clock();
-  printf("Time measured: %.3f seconds.\n",
-         ((double)(end - start)) / CLOCKS_PER_SEC);
+  printf("Time measured: %.3f seconds. Sum: %d\n",
+         ((double)(end - start)) / CLOCKS_PER_SEC, sum);
 
   start = clock();
+  sum = 0;
   for (int i = 0; i < N; i += 4) {
-    f(i);
-    f(i + 1);
-    f(i + 2);
-    f(i + 3);
+    sum += f(i);
+    sum += f(i + 1);
+    sum += f(i + 2);
+    sum += f(i + 3);
   }
   end = clock();
-  printf("Time measured: %.3f seconds.\n",
-         ((double)(end - start)) / CLOCKS_PER_SEC);
+  printf("Time measured: %.3f seconds. Sum: %d\n",
+         ((double)(end - start)) / CLOCKS_PER_SEC, sum);
 }
